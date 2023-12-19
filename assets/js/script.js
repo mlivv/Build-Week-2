995d31e2c45a8b2d62899abc84be0ae12fbea0ec
paolagentile01 <paolagentile01@libero.it>
2023-12-19 14:05:33 +0100

    modifiche

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,7 +81,7 @@ function topRanked(closestSongsArray){
   let number = 1;
   closestSongsArray.forEach(element =>{
     popularSongsContainer.innerHTML += `
-    <div class="song-block d-flex align-items-center justify-content-between" onmouseover=" playButton(this)"  onmouseout="returnNormal(this)" onclick=" setSongBar(this, '\${element.title}', '\${element.artist.name}','${element.album.cover}','${element.preview}')">
+    <div class="song-block d-flex align-items-center justify-content-between" onmouseover=" playButton(this)"  onmouseout="returnNormal(this)" onclick=" setSongBar(this, '${element.title}', '${element.artist.name}','${element.album.cover}','${element.preview}')">
     <div class="element-heading d-flex align-items-center gap-3">
         <span class="fs-4 number">
           ${number} 
@@ -118,7 +118,7 @@ function topRankedCollapsed(closestSongsArray){
   let number = 6;
   closestSongsArray.forEach(element =>{
     popularSongsContainer.innerHTML += `
-    <div class="song-block d-flex align-items-center justify-content-between" onmouseover=" playButton(this)"  onmouseout="returnNormal(this)" onclick=" setSongBar(this, '\${element.title}', '\${element.artist.name}','${element.album.cover}','${element.preview}')">
+    <div class="song-block d-flex align-items-center justify-content-between" onmouseover=" playButton(this)"  onmouseout="returnNormal(this)" onclick=" setSongBar(this, '${element.title}', '${element.artist.name}','${element.album.cover}','${element.preview}')">
     <div class="element-heading d-flex align-items-center gap-3">
         <span class="fs-4 number">
           ${number} 
@@ -167,8 +167,8 @@ function topRankedCollapsed(closestSongsArray){
       lastElement = currentElement;
   
       document.getElementById("album-cover-bar").innerHTML = `<img src="${cover}" alt="artist-album-image" height="60px">`;
-      document.getElementById("title-artist-bar").innerHTML = name;
-      document.getElementById("title-song-bar").innerHTML = title;
+      document.getElementById("title-artist-bar").innerText = name;
+      document.getElementById("title-song-bar").innerText = title;
       document.getElementById("current-audio-played").innerHTML = `<audio src="${previewAudio}" class="audio"></audio>`;
     }
     
